82c6c29a5b5257269de925ec57f3d2d9b9face48
George Carvalho <geocarvalho@outloo.com>
2019-06-29 19:35:14 -0300

    Finished first and second hypothesis

--- stat_project/firs_hip.R
+++ stat_project/firs_hip.R
@@ -1,6 +1,12 @@
+set.seed(1234)
+
 # Open file
 df <- read.csv("/home/george/Git/IN1100/stat_project/suicidio_prop_100k_hab_mes_2015-2017.csv")
 
 # Transpose columns as rows
 t_df <- setNames(data.frame(t(df[,-1])), df[,1])
 
+# Normality test with Shapiro-Wilk
+shapiro.test(t_df$'2015')
+shapiro.test(t_df$'2016')
+shapiro.test(t_df$'2017')
